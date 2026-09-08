862440b9dedf7419f1d5d01a5450d3f897090395
FilipMarkovi <fm54603@fer.hr>
2026-09-08 16:00:29 +0200

    fix prod

--- ageofhexes-io/server/src/index.ts
+++ ageofhexes-io/server/src/index.ts
@@ -194,7 +194,7 @@ const wss = new WebSocketServer({
       origin.includes("html.itch.zone") ||
       origin.includes("https://www.crazygames.com") ||
       origin.includes("https://files.crazygames.com") ||
-      origin.includes("https://developer.crazygames.com") ||
+      origin.includes("https://developer.crazygames.com")
     ) {
       const ip = getClientIp(info.req);
       if ((ipConnectionCounts.get(ip) || 0) >= MAX_WS_PER_IP) {
